c910919ef67e6523a8aca7c102c8046085c23729
Prithviraj <prithvirajmahamulkar100@gmail.com>
2023-07-20 10:48:11 +0530

    qwerty

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,3 +1,4 @@
+import { cookies } from "next/dist/client/components/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
@@ -9,7 +10,9 @@ export async function GET() {
             },
         )
 
-        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+        // Clear the token cookie
+        // cookies().set("token", "", { httpOnly: true, expires: new Date(0) });
+        // response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
         return response;
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
